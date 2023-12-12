7a1e3b5ad4be4be7c1acd29d016c0bda62b1e1fc
petrovyuri <mr.developer@yandex.ru>
2023-12-12 14:36:41 +0300

    add camera

---
+++ aurora/flutter/generated_plugin_registrant.cpp
@@ -0,0 +1,16 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include <flutter/application.h>
+#include <camera_aurora/camera_aurora_plugin.h>
+
+#include "generated_plugin_registrant.h"
+
+void RegisterPlugins() {
+    Application::RegisterPlugins({
+        std::make_shared<CameraAuroraPlugin>(),
+    });
+}
